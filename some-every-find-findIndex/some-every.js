ca54626383ad13a4414e5b9cedbc041634e0bacb
Nathan Soucier <soucienp@gmail.com>
2024-02-26 20:41:29 -0500

    added functions practicing with some and every

--- some-every-find-findIndex/some-every.js
+++ some-every-find-findIndex/some-every.js
@@ -1,45 +1,70 @@
 /*
-Write a function called hasOddNumber which accepts an array and returns true if the array contains at least one odd number, otherwise it returns false.
+Write a function called hasOddNumber which accepts an array and returns true if the array contains at
+least one odd number, otherwise it returns false.
 
 Examples:
     hasOddNumber([1,2,2,2,2,2,4]) // true
     hasOddNumber([2,2,2,2,2,4]) // false
 */
 
-function hasOddNumber(arr) {}
+function hasOddNumber(arr) {
+    return arr.some(function(val) {
+        return val % 2 
+    })
+}
 
 /*
-Write a function called hasAZero which accepts a number and returns true if that number contains at least one zero. Otherwise, the function should return false
+Write a function called hasAZero which accepts a number and returns true if that number contains
+at least one zero. Otherwise, the function should return false
 
 Examples:
     hasAZero(3332123213101232321) // true
     hasAZero(1212121) // false
 */
 
-function hasAZero(num) {}
+function hasAZero(num) {
+    const numArray = Array.from(num.toString());
+    return numArray.some(function(val) {
+        return val == 0;
+    }) 
+}
 
 /*
-Write a function called hasOnlyOddNumbers which accepts an array and returns true if every single number in the array is odd. If any of the values in the array are not odd, the function should return false. 
+Write a function called hasOnlyOddNumbers which accepts an array and returns true if every single
+number in the array is odd. If any of the values in the array are not odd, the function should return
+false. 
 
 Examples:
     hasOnlyOddNumbers([1,3,5,7]) // true
     hasOnlyOddNumbers([1,2,3,5,7]) // false
 */
 
-function hasOnlyOddNumbers(arr) {}
+function hasOnlyOddNumbers(arr) {
+    return arr.every(function(val) {
+        return val % 2;
+    })
+}
 
 /*
-Write a function called hasNoDuplicates which accepts an array and returns true if there are no duplicate values (more than one element in the array that has the same value as another). If there are any duplicates, the function should return false.
+Write a function called hasNoDuplicates which accepts an array and returns true if there are no
+duplicate values (more than one element in the array that has the same value as another). If there
+are any duplicates, the function should return false.
 
 Examples:
     hasNoDuplicates([1,2,3,1]) // false
     hasNoDuplicates([1,2,3]) // true
 */
 
-function hasNoDuplicates(arr) {}
+function hasNoDuplicates(arr) {
+    return arr.every(function(val) {
+        arr.splice(arr.indexOf(val), 1);
+        return arr.indexOf(val)===-1;
+    })
+}
 
 /*
-Write a function called hasCertainKey which accepts an array of objects and a key, and returns true if every single object in the array contains that key. Otherwise it should return false.
+Write a function called hasCertainKey which accepts an array of objects and a key, and returns true
+if every single object in the array contains that key. Otherwise it should return false.
 
 Examples:
     var arr = [
@@ -53,10 +78,14 @@ Examples:
     hasCertainKey(arr,'isCatOwner') // false
 */
 
-function hasCertainKey(arr, key) {}
+function hasCertainKey(arr, key) {
+    // finished off here
+}
 
 /*
-Write a function called hasCertainValue which accepts an array of objects and a key, and a value, and returns true if every single object in the array contains that value for the specific key. Otherwise it should return false.
+Write a function called hasCertainValue which accepts an array of objects and a key, and a value, and
+returns true if every single object in the array contains that value for the specific key. Otherwise
+it should return false.
 
 Examples:
     var arr = [
